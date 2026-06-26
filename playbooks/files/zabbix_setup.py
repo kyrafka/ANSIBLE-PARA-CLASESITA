24d42823617655d3db51996fffbdaafc24525f31
Jose Zuñiga <josedario.zuniga@gmail.com>
2026-06-26 12:20:25 -0500

    ga

--- playbooks/files/zabbix_setup.py
+++ playbooks/files/zabbix_setup.py
@@ -149,7 +149,7 @@ upsert_action("AXIOM: Auto PowerOn VM en ESXi", {
     "name": "AXIOM: Auto PowerOn VM en ESXi",
     "eventsource": 0,
     "status": 0,
-    "esc_period": "30s",
+    "esc_period": "60",
     "filter": {
         "evaltype": 0,
         "conditions": [{"conditiontype": 4, "operator": 5, "value": "2"}]
